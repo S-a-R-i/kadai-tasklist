bfd2e9314dc7be151769070c5f293e22a3f5f6eb
rina.sato <srgitgit@gmail.com>
2019-08-22 12:21:36 +0900

    Add getAllTasks

---
+++ src/models/Tasks.java
@@ -0,0 +1,77 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+
+    @Entity
+    @NamedQueries({
+        @NamedQuery(
+                name = "getAllTasks",
+                query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC"
+                )
+    })
+    @Table(name = "tasks")
+    public class Tasks {
+        @Id
+        @Column(name = "id")
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer id;
+
+        @Column(name = "created_date", nullable = false)
+        private Timestamp create_date;
+
+        @Column(name = "updated_date", nullable = false)
+        private Timestamp updated_date;
+
+        @Column(name = "content" ,length = 225 , nullable = false)
+        private String content;
+
+        //getter/setter
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public Timestamp getCreate_date() {
+            return create_date;
+        }
+
+        public void setCreate_date(Timestamp create_date) {
+            this.create_date = create_date;
+        }
+
+        public Timestamp getUpdated_date() {
+            return updated_date;
+        }
+
+        public void setUpdated_date(Timestamp updated_date) {
+            this.updated_date = updated_date;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+
+
+
+
+    }
+
+
