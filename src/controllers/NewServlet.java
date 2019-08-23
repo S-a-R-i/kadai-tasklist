ebd790363df47d4e02b2eb6cce5410ce7aa28459
rina.sato <srgitgit@gmail.com>
2019-08-23 09:09:33 +0900

    Add NewServlet.java

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,19 +1,25 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Tasks;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class NewServlet
  */
-@WebServlet("/NewServlet")
+@WebServlet("/new")
 public class NewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,20 +28,35 @@ public class NewServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
+
+        //DTOクラスのインスタンス化
+        Tasks t = new Tasks();
+
+        //インスタンスの各フィールドに値をset
+        String content = "content";
+        t.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        t.setCreate_date(currentTime);
+        t.setUpdated_date(currentTime);
+
+
+        //データベースに保存
+        em.persist(t);
+        em.getTransaction().commit();
+
+        //自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(t.getId()).toString());
+        em.close();
+
+    }
 
 }
